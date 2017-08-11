aecdf2095686663565c395d6c8c41a6a19ee404f
Dovannys Hernández Morales <dovannys@hotmail.com>
2017-08-11 17:09:00 -0500

    Fixed problem on IE11 with mousewheel.

--- wwwroot/js/Site.js
+++ wwwroot/js/Site.js
@@ -1,1 +1,12 @@
 ﻿// Write your Javascript code.
+function myFunction(e) {
+    var evt = window.event || e;
+    evt.preventDefault();
+    var wd = event.wheelDelta;
+    var csp = window.pageYOffset;
+    window.scrollTo(0, csp - wd);
+}
+
+if (navigator.userAgent.match(/Trident\/7\./)) {
+    document.body.addEventListener("mousewheel", myFunction, false);
+}
