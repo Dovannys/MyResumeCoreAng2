a4d281e9de6cb85f74d7637a7bdca59270978c7b
Dovannys Hernández Morales <dovannys@hotmail.com>
2019-06-02 22:14:48 -0500

    Added some Test.

---
+++ ResumeCVCoreAng2.Test/LangAPIControllerTests.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ResumeCVCoreAng2.Controllers;
+using ResumeCVCoreAng2.Models;
+using System;
+
+namespace ResumeCVCoreAng2.Test
+{
+    [TestClass]
+    public class LangAPIControllerTests
+    {
+        [TestMethod]
+        public void GetAllRetrieveEnglish()
+        {
+            // Arrange
+            var env = new Mock<IHostingEnvironment>();
+            env.Setup(x => x.WebRootPath).Returns(@"C:\Proyectos\ResumeCV\ResumeCVCoreAng2\ResumeCVCoreAng2\wwwroot");
+            var obj = new LangAPIController(env.Object);
+            var expected = "en";
+            // Act
+            var jsonresult = obj.GetAll(expected);
+            MyResume result = (MyResume)jsonresult.Value;
+            // Assert
+            Assert.AreEqual(expected, result.Language);
+        }
+
+        [TestMethod]
+        public void GetAllRetrieveSpanish()
+        {
+            // Arrange
+            var env = new Mock<IHostingEnvironment>();
+            env.Setup(x => x.WebRootPath).Returns(@"C:\Proyectos\ResumeCV\ResumeCVCoreAng2\ResumeCVCoreAng2\wwwroot");
+            var obj = new LangAPIController(env.Object);
+            var expected = "es";
+            // Act
+            var jsonresult = obj.GetAll(expected);
+            MyResume result = (MyResume)jsonresult.Value;
+            // Assert
+            Assert.AreEqual(expected, result.Language);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetAllRetrieveErrorWithNullParameter()
+        {
+            // Arrange
+            var env = new Mock<IHostingEnvironment>();
+            env.Setup(x => x.WebRootPath).Returns(@"C:\Proyectos\ResumeCV\ResumeCVCoreAng2\ResumeCVCoreAng2\wwwroot");
+            var obj = new LangAPIController(env.Object);
+            string expected = null;
+            // Act
+            var jsonresult = obj.GetAll(expected);
+            MyResume result = (MyResume)jsonresult.Value;
+            // Assert           
+            //Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetAllRetrieveErrorWithEmptyParameter()
+        {
+            // Arrange
+            var env = new Mock<IHostingEnvironment>();
+            env.Setup(x => x.WebRootPath).Returns(@"C:\Proyectos\ResumeCV\ResumeCVCoreAng2\ResumeCVCoreAng2\wwwroot");
+            var obj = new LangAPIController(env.Object);
+            string expected = "";
+            // Act
+            var jsonresult = obj.GetAll(expected);
+            MyResume result = (MyResume)jsonresult.Value;
+            // Assert            
+            //Assert.IsNull(result);
+        }
+    }
+}
